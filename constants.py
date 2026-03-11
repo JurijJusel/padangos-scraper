cb2ff6b220da500beea973dbe35f985abe271e11
Jurij Jusel <jjusel@gmail.com>
2026-03-11 22:53:20 +0200

    start

---
+++ constants.py
@@ -0,0 +1,15 @@
+# URL to scrape tires from.
+
+URL_123 = "https://padangos123.lt/"
+
+URL_LYDERIS = "https://www.padangulyderis.lt/"
+
+URL_MELGA = "https://melga.lt/"
+
+URL_PLIUS = "https://padangosplius.lt/"
+
+URL_PARDUOTUVE = "https://www.padanguparduotuve.lt/"
+
+URL_VISOS = "https://www.visos-padangos.lt/"
+
+URL_G = "https://www.gpadangos.lt/"
